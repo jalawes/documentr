a04aebf688dea8f6cb58a4ec00730c4d58eea289
Jamal <jamal.alawes@gmail.com>
2017-12-21 00:35:47 -0600

    user can join channels

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function channels()
+    {
+        return $this->belongsToMany(Channel::class);
+    }
+
+    public function join($channel)
+    {
+        return $this->channels()->attach($channel);
+    }
 }
