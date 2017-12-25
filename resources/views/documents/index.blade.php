8ad013983f0309921367e3c36edcdf99ae2cc1ae
Jamal <jamal.alawes@gmail.com>
2017-12-25 03:50:41 -0600

    updates docs table

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <h1 class="title">Documents</h1>
 
-    <table class="table is-bordered is-striped is-narrow is-fullwidth">
-        <tr>
-            <th>Title</th>
-            <th class="has-text-right">Created</th>
-        </tr>
-        @foreach($documents as $document)
-        <tr>
-            <td>
-                <icon name="file-text-o"></icon>
-                <a href="{{ $document->path() }}">{{ $document->filename }}</a></td>
-            <td class="has-text-right">{{ $document->created_at->diffForHumans() }}</td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">Documents</p>
+        </header>
+
+        <div class="card-content">
+            <table class="table is-fullwidth is-hoverable">
+                <thead>
+                    <tr>
+                        <th>File</th>
+                        <th class="has-text-right">Created</th>
+                    </tr>
+                </thead>
+                @foreach($documents as $document)
+                <tr>
+                    <td>
+                        <icon name="file-text-o"></icon>
+                        <a href="{{ $document->path() }}">{{ $document->filename }}</a></td>
+                    <td class="has-text-right">{{ $document->created_at->diffForHumans() }}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+<br>
 </div>
 
 @endsection
