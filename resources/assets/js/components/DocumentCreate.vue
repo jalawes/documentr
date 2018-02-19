75aabe6845bf4cb03a0d917f6395cf871fff3233
Jamal <jamal.alawes@gmail.com>
2018-02-19 16:16:43 -0600

    refactoring conditions

--- resources/assets/js/components/DocumentCreate.vue
+++ resources/assets/js/components/DocumentCreate.vue
@@ -1,48 +1,58 @@
 <template>
-<form @submit.prevent="submit">
-  <div class="field">
-    <label class="label" for="title">Title</label>
+  <form @submit.prevent="submit">
+    <!--Title-->
+    <div class="field">
+      <label class="label" for="title">Title</label>
       <div class="control has-icons-left">
-          <input class="input"
-                 type="text"
-                 name="title"
-                 placeholder="My New Document"
-                 required
-                 v-model="title">
-          <span class="icon is-small is-left"><i class="fa fa-file-text-o"></i></span>
+        <input class="input"
+               type="text"
+               id="title"
+               name="title"
+               placeholder="My New Document"
+               required
+               v-model="title">
+        <span class="icon is-small is-left"><i class="fa fa-file-text-o"></i></span>
       </div>
     </div>
+
+    <!--Body-->
     <div class="field">
-        <label class="label" for="body">Body</label>
-        <div class="control has-icons-left has-icons-right">
-          <code-mirror name="body" v-model="body" v-show="!showPreview"></code-mirror>
-          <preview :code="body" v-if="showPreview"></preview>
-        </div>
+      <label class="label" for="body">Body</label>
+      <div class="control has-icons-left has-icons-right">
+        <code-mirror id="body" name="body" v-model="body" v-show="!showPreview"></code-mirror>
+        <preview :code="body" v-if="showPreview"></preview>
+      </div>
     </div>
+
+    <!--Button panel-->
     <div class="buttons is-right">
+      <fade-in>
         <button class="button is-white tooltip"
                 data-tooltip="Toggle Preview"
                 v-if="canReset"
                 @click.prevent="togglePreview">
-          <icon :name="showPreview ? 'eye' : 'eye-slash'"></icon>
+          <icon :name="showPreview ? 'eye-slash' : 'eye'"></icon>
         </button>
-        <fade-in>
-          <button class="button is-white tooltip"
-                  data-tooltip="Reset"
-                  type="reset"
-                  v-if="canReset"
-                  @click.prevent="reset">
-              <icon name="undo"></icon>
-          </button>
-        </fade-in>
-        <fade-in>
-          <button class="button is-white tooltip"
-                  data-tooltip="Send"
-                  type="submit"
-                  v-if="canSubmit">
-              <icon name="send-o"></icon>
-          </button>
-        </fade-in>
+      </fade-in>
+
+      <fade-in>
+        <button class="button is-white tooltip"
+                data-tooltip="Reset"
+                type="reset"
+                v-if="canReset"
+                @click.prevent="reset">
+          <icon name="undo"></icon>
+        </button>
+      </fade-in>
+
+      <fade-in>
+        <button class="button is-white tooltip"
+                data-tooltip="Send"
+                type="submit"
+                v-if="canReset">
+          <icon name="send-o"></icon>
+        </button>
+      </fade-in>
     </div>
   </form>
 </template>
@@ -50,49 +60,61 @@
 <script>
   export default {
     name: 'DocumentCreate',
+
     methods: {
+      /**
+       * Return true if the document has the specified field filled
+       */
+      hasField(field) {
+        return _.isString(field) && !_.isEmpty(field);
+      },
+
+      /**
+       * Reset the input fields
+       */
       reset() {
         this.title = '';
         this.body = '';
         this.library_id = '';
       },
+
+      /**
+       * Submit the form
+       */
       submit() {
         axios.post('/documents', {
-          title: this.title,
-          body: this.body,
+          title:      this.title,
+          body:       this.body,
           library_id: this.library_id,
         }).then((response) => {
           flash('Published ' + this.title + '!');
-        }).catch(function (error) {
+        }).catch(function(error) {
           flash('Error');
+          console.log(error);
         });
       },
 
+      /**
+       * Toggle display of the preview
+       */
       togglePreview() {
-        this.showPreview = !this.showPreview
-      }
+        this.showPreview = !this.showPreview;
+      },
     },
-    data () {
+
+    data() {
       return {
         title: '',
         body: '',
         library_id: null,
         showPreview: false,
-      }
+      };
     },
+
     computed: {
       canReset() {
-        return this.hasBody || this.hasTitle
+        return this.hasField(this.body) || this.hasField(this.title);
       },
-      canSubmit() {
-        return this.canReset
-      },
-      hasBody() {
-        return _.isString(this.body) && !_.isEmpty(this.body);
-      },
-      hasTitle() {
-        return _.isString(this.title) && !_.isEmpty(this.title);
-      }
-    }
-  }
+    },
+  };
 </script>
